a59da39b49f324779750fb50fc44d5b759573be8
ShoryaThakur <thakurshorya417@gmail.com>
2023-07-01 18:01:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let containerEL = document.querySelector(".container");
-for (let index = 0; index < 640; index++) {
+for (let index = 0; index < 648; index++) {
   let containEL = document.createElement("div");
   containEL.classList.add("contain");
   containerEL.appendChild(containEL);
