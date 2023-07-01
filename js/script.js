fa21f85e21484369120ca02271e41109f1f896e8
ShoryaThakur <thakurshorya417@gmail.com>
2023-07-01 17:44:29 +0530

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 let containerEL = document.querySelector(".container");
 for (let index = 0; index < 32; index++) {
-  const containEL = document.createElement("div");
+  let containEL = document.createElement("div");
   containEL.classList.add("contain");
   containerEL.appendChild(containEL);
+  const chars = "0123456789abcdef";
+//   console.log(chars.length);
+  const colorcodelength = 6;
+  let colorcode = "";
+  for (let j = 0; j < colorcodelength; j++) {
+    let randomNum = Math.floor(Math.random() * chars.length);
+    colorcode += chars.slice(randomNum, randomNum + 1);
+    //   console.log(colorcode);
+  }
+  colorcode = "#" + colorcode;
+  containEL.innerText = colorcode;
+  containEL.style.backgroundColor = colorcode;
 }
